d946a36860ea96a54f55180958ab9d791e987c17
subham <subhamkashyapp@gmail.com>
2022-02-16 16:33:50 +0530

    jsx

---
+++ jsxExample.jsx
@@ -0,0 +1,17 @@
+const OnboardingJsx = <div>On Onboarding</div>
+
+// here h1 is not a html element its an jsx tag 
+// which get convert into real html element using document.createElement api
+
+// below syntax shows how jsx is embeded inside react component
+
+
+const h1Component = () => {
+
+    return <h1>hi i am a jsx inside react functional component</h1>      
+}
+
+
+
+
+
